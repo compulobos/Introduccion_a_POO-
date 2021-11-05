7790512b7b7abd5f732df98cc5f09621ec7eaafe
alxdx <ustongs09@gmail.com>
2021-11-04 20:52:29 -0600

    sesion 4 de noviembre de 2021, clase aplicacionCalculadora y Calculadora creada

---
+++ Calculadora.java
@@ -0,0 +1,15 @@
+public class Calculadora{
+	public int suma(int numeroA,int  numeroB){
+		return numeroA +numeroB;
+	}
+	public int resta(int numeroA,int numeroB){
+		if (revisarSiRestaEsPosible()){
+
+		}else{
+			return -1;
+		}
+	}
+	private static revisarSiRestaEsPosible(int numeroA,int numeroB){
+		
+	}
+}
---
+++ aplicacionCalculadora.java
@@ -0,0 +1,21 @@
+public class aplicacionCalculadora {
+	private void mostrarMenu(){
+		System.out.println("que deseas hacer?\n 1 : resta \n 2:sumar");
+	}
+	public static void main(String[] args) {
+		//new aplicacionCalculadora().mostrarMenu();
+		Calculadora calculadora1 = new Calculadora();
+
+		int resultado = calculadora1.suma(8,10);
+		System.out.println(resultado);
+	}
+}
+
+//Cuando definimos un método debe de ir acompañado del acceso a la clase y 
+//del tipo de dato que va a regresar
+/*
+public (cualquiera lo puede utilizar)
+private (solo la clase lo puede utilizar)
+protected son palabras que se refieren al encapsulamiento (solo algunas clases)
+*/
+//static -> significa que no necesito instanciar un objeto para poder usarlo
